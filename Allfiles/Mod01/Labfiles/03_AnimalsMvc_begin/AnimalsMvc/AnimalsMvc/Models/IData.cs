39afdc1d5735e5c57ba75c16367e0c6ddbebea31
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-02-05 16:59:59 -0600

    module 1 lab exercise 3

---
+++ Allfiles/Mod01/Labfiles/03_AnimalsMvc_begin/AnimalsMvc/AnimalsMvc/Models/IData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AnimalsMvc.Models
+{
+    public interface IData
+    {
+        List<Animal> AnimalsList { get; set; }
+
+        List<Animal> AnimalsInitializeData();
+
+        Animal GetAnimalById(int? id);
+    }
+}

5a148d3c8a3fa386b7c7a73426e720c398d6697a
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-02-13 22:01:17 -0600

    module 5 lab exercise 3

---
+++ Allfiles/Mod05/Labfiles/01_CitiesWebsite_begin/CitiesWebsite/ViewComponents/CityViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using CitiesWebsite.Models;
+using CitiesWebsite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CitiesWebsite.ViewComponents
+{
+    public class CityViewComponent : ViewComponent
+    {
+        private readonly ICityProvider _cities;
+
+        public CityViewComponent(ICityProvider cities)
+        {
+            _cities = cities;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string cityName)
+        {
+            ViewBag.CurrentCity = await GetCity(cityName);
+            return View("SelectCity");
+        }
+
+        private Task<City> GetCity(string cityName)
+        {
+            return Task.FromResult(_cities[cityName]);
+        }
+    }
+}

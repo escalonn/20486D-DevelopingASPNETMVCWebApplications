4a0cbdcd4fd0346072507d0ec8d5f15095beb088
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-02-16 04:43:35 -0600

    module 8 lab exercise 3

---
+++ Allfiles/Mod08/Labfiles/01_ZooSite_begin/ZooSite/wwwroot/js/form-functions.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+function calculateSum() {
+  var rows = document.querySelectorAll('#totalAmount tr .sum');
+  var sum = 0;
+  for (var i = 0; i < rows.length; i++) {
+    sum += parseFloat(parseFloat(rows[i].innerHTML.substring(1, rows[i].innerHTML.length)).toFixed(2));
+  }
+  document.getElementById('sum').innerHTML = 'Total: $' + sum;
+}

53bd363ca74e53c325c9b6f0b51ec08cfc0c2b2e
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-02-23 06:15:00 -0600

    module 13 lab exercise 1

---
+++ Allfiles/Mod13/Labfiles/01_Restaurant_begin/Server/Controllers/ReservationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Data;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly RestaurantContext _context;
+
+        public ReservationController(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<OrderTable> GetById(int id)
+        {
+            if (_context.ReservationsTables.FirstOrDefault(p => p.Id == id) is OrderTable order)
+            {
+                return order;
+            }
+            return NotFound();
+        }
+
+        [HttpPost("{id}")]
+        public ActionResult<OrderTable> Create(OrderTable orderTable)
+        {
+            _context.Add(orderTable);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), routeValues: new { id = orderTable.Id }, value: orderTable);
+        }
+    }
+}
---
+++ Allfiles/Mod13/Labfiles/01_Restaurant_begin/Server/Controllers/RestaurantBranchesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Server.Data;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantBranchesController : ControllerBase
+    {
+        private readonly RestaurantContext _context;
+
+        public RestaurantBranchesController(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<RestaurantBranch>> Get()
+        {
+            var branches = _context.RestaurantBranches
+                .OrderBy(r => r.City);
+            return branches.ToList();
+        }
+    }
+}
